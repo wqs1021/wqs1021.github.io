03f4240f66e60b85c1323cd8db5ef47160cd0c64
wqs <1021wqs@gmail.com>
2026-07-11 08:50:13 +0800

    Disable unstable service worker on Apple devices

--- service-worker.js
+++ service-worker.js
@@ -1,66 +1,13 @@
-const CACHE_NAME = 'personal-workout-log-v12';
-const CORE_ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './assets/app.css',
-  './assets/icon-192.png',
-  './assets/icon-512.png',
-  './assets/vendor/fontawesome/css/all.min.css',
-  './assets/vendor/fontawesome/webfonts/fa-solid-900.woff2',
-  './assets/vendor/chart.umd.min.js'
-];
-
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(CORE_ASSETS))
-      .then(() => self.skipWaiting())
-  );
+  event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  const request = event.request;
-  if (request.method !== 'GET') return;
-  const url = new URL(request.url);
-  if (url.origin !== self.location.origin) return;
-
-  if (request.mode === 'navigate') {
-    event.respondWith(
-      fetch(request)
-        .then(response => {
-          if (response.ok) {
-            const copy = response.clone();
-            caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy));
-          }
-          return response;
-        })
-        .catch(() => caches.match('./index.html'))
-    );
-    return;
-  }
-
-  event.respondWith(
-    caches.match(request).then(cached => {
-      const fetchPromise = fetch(request)
-        .then(response => {
-          if (response && response.ok) {
-            const copy = response.clone();
-            caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
-          }
-          return response;
-        })
-        .catch(() => cached);
-
-      return cached || fetchPromise;
-    })
+      .then(keys => Promise.all(keys
+        .filter(key => key.startsWith('personal-workout-log-'))
+        .map(key => caches.delete(key))))
+      .then(() => self.registration.unregister())
   );
 });
